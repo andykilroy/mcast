adcb69dfae2f96f8f6da05115dff47372c334f91
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2016-12-08 23:48:23 +0000

    parse args correctly

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,56 @@
 use std::net::UdpSocket;
+use std::net::AddrParseError;
 use std::net::ToSocketAddrs;
 use std::net::SocketAddr;
 use std::net::SocketAddrV4;
 use std::net::Ipv4Addr;
+use std::error::Error;
+use std::str::FromStr;
+use std::clone::Clone;
+use std::marker::Copy;
 use std::thread;
 
+#[derive(Copy)]
+struct Params {
+    send_only : bool,
+    grp_addr : Ipv4Addr,
+    grp_port : u16,
+    nic : Ipv4Addr,
+}
+
+impl Clone for Params {
+    fn clone(&self) -> Params { *self }
+}
+
+
 fn main() {
-    let send_only = false;
-    let grp_addr = Ipv4Addr::new(224, 0, 0, 1);
-    let grp_port = 9473u16;
-    let nic = Ipv4Addr::new(127, 0, 0, 1);
+    let params = read_params().unwrap();
 
-
-    let grp_sock_addr = SocketAddrV4::new(grp_addr, grp_port);
+    let grp_sock_addr = SocketAddrV4::new(params.grp_addr, params.grp_port);
     let any = Ipv4Addr::new(0, 0, 0, 0);
 
-    let bind_sock_addr = SocketAddrV4::new(any, grp_port);
-    if !send_only {
+    let bind_sock_addr = SocketAddrV4::new(any, params.grp_port);
+    if !params.send_only {
         thread::Builder::new().name("mcast_reader".to_string()).spawn(move || {
             mcast_reader(
                 &(bind_sock_addr.to_string()),
-                &grp_addr,
-                &nic
+                &params.grp_addr,
+                &params.nic
             );
         });
     }
 
+    read_from_stdin(params.nic, grp_sock_addr);
+}
 
-    read_from_stdin(nic, grp_sock_addr);
+fn read_params() -> Result<Params, AddrParseError> {
+    let args: Vec<String> = std::env::args().collect();
+    let send_only = bool::from_str(&args[1]).unwrap();
+    let grp = Ipv4Addr::from_str(&args[2]).unwrap();
+    let port = u16::from_str(&args[3]).unwrap();
+    let nic = Ipv4Addr::from_str(&args[4]).unwrap();
+
+    Ok(Params {send_only: send_only, grp_addr: grp, grp_port: port, nic: nic})
 }
 
 fn read_from_stdin(nic: Ipv4Addr, grp_sock_addr: SocketAddrV4) {
