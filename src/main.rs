f4a35a0ffb04d6064eb62324ab0aac6828538cb5
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2016-12-09 00:58:07 +0000

    cosmetic clean up

--- src/main.rs
+++ src/main.rs
@@ -11,38 +11,41 @@ use std::clone::Clone;
 use std::marker::Copy;
 use std::thread;
 
+
+
 #[derive(Copy)]
 struct Params {
-    send_only : bool,
-    grp_addr : Ipv4Addr,
-    grp_port : u16,
-    nic : Ipv4Addr,
+    send_only: bool,
+    grp_sock_addr: SocketAddrV4,
+    nic: Ipv4Addr,
 }
+
 
 impl Clone for Params {
     fn clone(&self) -> Params { *self }
 }
 
 
+
 fn main() {
     let params = read_params().unwrap();
-
-    let grp_sock_addr = SocketAddrV4::new(params.grp_addr, params.grp_port);
     let any = Ipv4Addr::new(0, 0, 0, 0);
+    let bind_sock_addr = SocketAddrV4::new(any, params.grp_sock_addr.port());
 
-    let bind_sock_addr = SocketAddrV4::new(any, params.grp_port);
     if !params.send_only {
         thread::Builder::new().name("mcast_reader".to_string()).spawn(move || {
-            mcast_reader(
-                &(bind_sock_addr.to_string()),
-                &params.grp_addr,
+            mcast_reader_v4(
+                &bind_sock_addr,
+                &params.grp_sock_addr.ip(),
                 &params.nic
             );
         });
     }
 
-    read_from_stdin(params.nic, grp_sock_addr);
+    read_from_stdin(params.nic, params.grp_sock_addr);
 }
+
+
 
 fn read_params() -> Result<Params, AddrParseError> {
     let args: Vec<String> = std::env::args().collect();
@@ -51,14 +54,16 @@ fn read_params() -> Result<Params, AddrParseError> {
     let port = u16::from_str(&args[3]).unwrap();
     let nic = Ipv4Addr::from_str(&args[4]).unwrap();
 
-    Ok(Params {send_only: send_only, grp_addr: grp, grp_port: port, nic: nic})
+    Ok(Params {send_only: send_only, grp_sock_addr: SocketAddrV4::new(grp, port), nic: nic})
 }
 
+
+
 fn read_from_stdin(nic: Ipv4Addr, grp_sock_addr: SocketAddrV4) {
-    
     let mut snd_sock = UdpSocket::bind(SocketAddrV4::new(nic, 0)).unwrap();
     let mut from_in = String::new();
     let istream = std::io::stdin();
+
     loop {
         istream.read_line(&mut from_in);
         snd_sock.send_to(from_in.as_bytes(), grp_sock_addr);
@@ -66,9 +71,11 @@ fn read_from_stdin(nic: Ipv4Addr, grp_sock_addr: SocketAddrV4) {
     }
 }
 
-fn mcast_reader(bindaddr: &str,
-                mcastgrp: &Ipv4Addr,
-                interface: &Ipv4Addr) {
+
+
+fn mcast_reader_v4(bindaddr:  &SocketAddrV4,
+                   mcastgrp:  &Ipv4Addr,
+                   interface: &Ipv4Addr      ) {
     let mut socket = UdpSocket::bind(bindaddr).unwrap();
     let mut rcv_buf: [u8; 65536] = [0; 65536];
 
@@ -77,7 +84,7 @@ fn mcast_reader(bindaddr: &str,
         let (byte_count, sender) = socket.recv_from(&mut rcv_buf[..]).unwrap();
         let sl : &[u8] = &rcv_buf[0..byte_count];
         let s = str::from_utf8(sl).unwrap();
-        print!("{}", s);
+        print!("from {} rcvd {}", sender, s);
     }
 }
 
