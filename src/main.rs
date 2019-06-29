f171234315337090103e86b2b0fb6077fd302575
Andrew Kilroy <7545475+andykilroy@users.noreply.github.com>
2019-06-29 20:46:35 +0100

    make use of clap to document cmd line arguments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,11 @@ use std::net::Ipv4Addr;
 use std::str::FromStr;
 use std::str;
 use std::io;
+use std::process;
 use socket2::{SockAddr, Socket, Domain, Type, Protocol};
+
+use clap::{App, Arg, ArgMatches, SubCommand};
+use std::io::ErrorKind;
 
 
 struct Params {
@@ -41,24 +45,88 @@ impl FromStr for Command {
 
 
 fn main() {
-    match read_params() {
-        Ok(params) => use_parameters(params),
-        Err(e) => eprintln!("Could not parse params, {}", e)
+    if let Err(e) = start_app() {
+        eprintln!("{}", e);
+        process::exit(1);
     };
 }
 
-fn read_params() -> std::result::Result<Params, String> {
-    let usage_string = "Usage: mcast send|listen <group:ip> <port:int> <nic:ip>";
-    let args: Vec<String> = std::env::args().collect();
-    if args.len() < 5 {
-        return Err(format!("expected more\n{}", usage_string))
-    }
-    let act = Command::from_str(&args[1]).map_err(|_| usage_string)?;
-    let grp = Ipv4Addr::from_str(&args[2]).map_err(|_| usage_string)?;
-    let port = u16::from_str(&args[3]).map_err(|_| usage_string)?;
-    let nic = Ipv4Addr::from_str(&args[4]).map_err(|_| usage_string)?;
+fn start_app() -> std::result::Result<(), String> {
+    let matches = App::new(env!("CARGO_PKG_NAME"))
+        .version(env!("CARGO_PKG_VERSION"))
+        .about("A tool for testing multicast UDP")
+        .subcommand(
+            SubCommand::with_name("listen")
+                .about("Listen on a particular network interface for datagrams from a multicast group")
+                .arg(
+                    Arg::with_name("GROUP_IP")
+                        .help("The multicast group to join")
+                        .required(true)
+                )
+                .arg(
+                    Arg::with_name("PORT")
+                        .help("The port to bind on")
+                        .required(true)
+                )
+                .arg(
+                    Arg::with_name("NIC_IP")
+                        .help("The network interface on which to send the join requests")
+                        .required(true)
+                )
+        )
+        .subcommand(
+            SubCommand::with_name("send")
+                .about("Send datagrams to a multicast group via a particular network interface")
+                .arg(
+                    Arg::with_name("GROUP_IP")
+                        .help("The multicast group to send to")
+                        .required(true)
+                )
+                .arg(
+                    Arg::with_name("PORT")
+                        .help("The destination port to send to")
+                        .required(true)
+                )
+                .arg(
+                    Arg::with_name("NIC_IP")
+                        .help("The network interface on which to send the datagrams")
+                        .required(true)
+                )
+        ).get_matches();
 
-    Ok(Params {action: act, grp_sock_addr: SocketAddrV4::new(grp, port), nic: nic})
+    match matches.subcommand() {
+        ("listen", Some(subm)) => {
+            handle_listen(
+                &subm.value_of("GROUP_IP").expect("a multicast group was expected"),
+                &subm.value_of("PORT").expect("a port number was expected"),
+                &subm.value_of("NIC_IP").expect("a nic was expected"),
+            )
+        },
+        ("send", Some(subm)) => {
+            handle_send(
+                &subm.value_of("GROUP_IP").expect("a multicast group was expected"),
+                &subm.value_of("PORT").expect("a port number was expected"),
+                &subm.value_of("NIC_IP").expect("a nic was expected"),
+            )
+        },
+        (cmd, _) => Err("unsupported command, try --help option".to_string())
+    }
+}
+
+fn handle_listen(grp_str: &str, port_str: &str, nic_str: &str) -> Result<(), String> {
+    let any = Ipv4Addr::new(0, 0, 0, 0);
+    let port = u16::from_str(port_str).map_err(|e| format!("could not parse port number {}, {}", port_str, e))?;
+    let grp = Ipv4Addr::from_str(grp_str).map_err(|e| format!("could not parse group address {}, {}", grp_str, e))?;
+    let nic = Ipv4Addr::from_str(nic_str).map_err(|e| format!("could not parse nic address {}, {}", nic_str, e))?;
+    let bind_sock_addr = SocketAddrV4::new(any, port);
+    mcast_reader_v4(&bind_sock_addr, &grp, &nic).map_err(|e| format!("{}", e))
+}
+
+fn handle_send(grp_str: &str, port_str: &str, nic_str: &str) -> Result<(), String> {
+    let port = u16::from_str(port_str).map_err(|e| format!("could not parse port number {}, {}", port_str, e))?;
+    let grp = Ipv4Addr::from_str(grp_str).map_err(|e| format!("could not parse group address {}, {}", grp_str, e))?;
+    let nic = Ipv4Addr::from_str(nic_str).map_err(|e| format!("could not parse nic address {}, {}", nic_str, e))?;
+    send_to_mcast_socket(nic, SocketAddrV4::new(grp, port)).map_err(|e| format!("{}", e))
 }
 
 fn use_parameters(params: Params) {
